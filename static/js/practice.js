db538ce8f1d11d303ac928efbd18da72512682f0
AnnoyingRains <annoyingrain5@gmail.com>
2024-03-18 12:45:00 +1100

    Allow pressing number keys to select multiple-choice options

--- static/js/practice.js
+++ static/js/practice.js
@@ -10,17 +10,34 @@ function checkanswer(answer, correctanswer) {
 
 function init() {
     var input = document.getElementById("answer")
-    input.value = ""
-    input.focus()
-    input.addEventListener("keypress", function (event) {
-        // If the user presses the "Enter" key on the keyboard
-        if (event.key === "Enter") {
-            // Cancel the default action, if needed
-            event.preventDefault()
-            // Trigger the button element with a click
-            document.getElementById("submit").click()
-        }
-    })
+    // if this is an input subpage
+    if (input != null) {
+        input.value = ""
+        input.focus()
+        input.addEventListener("keypress", function (event) {
+            // If the user presses the "Enter" key on the keyboard
+            if (event.key === "Enter") {
+                // Cancel the default action, if needed
+                event.preventDefault()
+                // Trigger the button element with a click
+                document.getElementById("submit").click()
+            }
+        })
+    }
+    // if this is a multiple-choice subpage
+    else {
+        document.addEventListener("keypress", function (event) {
+            if (event.key === "1") {
+                document.getElementById("answer1").click()
+            }
+            else if (event.key === "2") {
+                document.getElementById("answer2").click()
+            }
+            else if (event.key === "3") {
+                document.getElementById("answer3").click()
+            }
+        })
+    }
 }
 
 window.onload = init;
